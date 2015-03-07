42a3bdef20ba3994557190647385981a608151f0
ZhihaoZhang <zz2323@columbia.edu>
2015-03-07 17:16:39 -0500

    added a markerto the map to the scripts.js.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
 var map = L.map('CHCMap').setView([40.7127, -74.005973], 13);
+
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+	attribution : '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+var marker = L.marker([40.73084, -74.006704]).addTo(map);
