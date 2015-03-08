94907187456f4a4520c17a62d97a15c9558c53a2
ZhihaoZhang <zz2323@columbia.edu>
2015-03-08 15:20:21 -0400

    added the } inscript that just saved my life.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
-var map = L.map('CHCMap').setView([40.7127, -74.005973], 13);//pointing the map to New York City
+var map = L.map('CHCMap').setView([40.7127, -74.005973], 13); //pointing the map to New York City
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-	attribution : '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 var HealthCenterArray = mappingHealthCenterData.rows; //creating an array for the data, pulling all the rows
@@ -11,9 +11,9 @@ var HealthCenterArray = mappingHealthCenterData.rows; //creating an array for th
  */
 
 for (var i = 0; i < HealthCenterArray.length; i++) {
-	
-	var EachLocation = HealthCenterArray[i];
-	
-var marker = L.marker([EachLocation.latitude, EachLocation. longitude]).addTo(map);//dotting the marker location
 
-marker.bindPopup("<b>"+ EachLocation.FullAddress+"</b><br>"+EachLocation.NameofCenter+"</b><br>"+EachLocation.TelephoneNumber);//what the popup has
+    var EachLocation = HealthCenterArray[i];
+
+    var marker = L.marker([EachLocation.latitude, EachLocation.longitude]).addTo(map); //dotting the marker location
+
+    marker.bindPopup("<b>" + EachLocation.FullAddress + "</b><br>" + EachLocation.NameofCenter + "</b><br>" + EachLocation.TelephoneNumber);} //what the popup has
