297dacb1e136b3b0a55537a9fd6ee466dc9f7070
ZhihaoZhang <zz2323@columbia.edu>
2015-03-07 19:19:32 -0500

    cleaned the captions and format the script page.

--- scripts.js
+++ scripts.js
@@ -1,23 +1,19 @@
-var map = L.map('CHCMap').setView([40.7127, -74.005973], 13);
+var map = L.map('CHCMap').setView([40.7127, -74.005973], 13);//pointing the map to New York City
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 	attribution : '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-var HealthCenterArray = mappingHealthCenterData.rows;
+var HealthCenterArray = mappingHealthCenterData.rows; //creating an array for the data, pulling all the rows
+
+/*
+ * A loop in which each location's full address, name, and telephone number are displayed in a blue popup'
+ */
 
 for (var i = 0; i < HealthCenterArray.length; i++) {
 	
 	var EachLocation = HealthCenterArray[i];
 	
-var marker = L.marker([EachLocation.latitude, EachLocation. longitude]).addTo(map);
+var marker = L.marker([EachLocation.latitude, EachLocation. longitude]).addTo(map);//dotting the marker location
 
-marker.bindPopup("<b>"+ EachLocation.FullAddress+"</b><br>"+EachLocation.NameofCenter+"</b><br>"+EachLocation.TelephoneNumber);
-
-	
-	
-	
-	
-	//var marker = L.marker([individualWarmingShelter.latitude, individualWarmingShelter.Longitude]).addTo(map);
-	//marker.bindPopup("<b>"+individualWarmingShelter.fullAddress+"</b><br>"+individualWarmingShelter.ProgramName);
-}//end of the function
+marker.bindPopup("<b>"+ EachLocation.FullAddress+"</b><br>"+EachLocation.NameofCenter+"</b><br>"+EachLocation.TelephoneNumber);//what the popup has
