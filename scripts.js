507ab44ce15054a9e858167a85d4c2c9ac2d6873
ZhihaoZhang <zz2323@columbia.edu>
2015-03-07 19:09:37 -0500

    added marker popup including name, full address and telephone.

--- scripts.js
+++ scripts.js
@@ -4,14 +4,20 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 	attribution : '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-var marker = L.marker([40.798664,-73.967778]).addTo(map);
-
 var HealthCenterArray = mappingHealthCenterData.rows;
 
 for (var i = 0; i < HealthCenterArray.length; i++) {
 	
 	var EachLocation = HealthCenterArray[i];
 	
-	var marker = L.marker([individualWarmingShelter.latitude, individualWarmingShelter.Longitude]).addTo(map);
-	marker.bindPopup("<b>"+individualWarmingShelter.fullAddress+"</b><br>"+individualWarmingShelter.ProgramName);
+var marker = L.marker([EachLocation.latitude, EachLocation. longitude]).addTo(map);
+
+marker.bindPopup("<b>"+ EachLocation.FullAddress+"</b><br>"+EachLocation.NameofCenter+"</b><br>"+EachLocation.TelephoneNumber);
+
+	
+	
+	
+	
+	//var marker = L.marker([individualWarmingShelter.latitude, individualWarmingShelter.Longitude]).addTo(map);
+	//marker.bindPopup("<b>"+individualWarmingShelter.fullAddress+"</b><br>"+individualWarmingShelter.ProgramName);
 }//end of the function
